37b6dd8907467649e63c7ee114121be90f62c61f
kenny-tse <kennytse91@gmail.com>
2021-10-07 22:36:02 +0000

    added support for smaller resolution and fixed navbar

--- public/scripts/sticky-navbar.js
+++ public/scripts/sticky-navbar.js
@@ -1,17 +1,35 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+// When the user scrolls the page, execute navSticky
+// window.onscroll = function () { navSticky() };
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function navSticky() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+// window.onscroll = function () { navStickyProfile() };
+
+// // Get the navbar
+// var navbarProfile = document.getElementById("header");
+
+// // Get the offset position of the navbar
+// var stickyProfile = navbarProfile.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function navStickyProfile() {
+//   if (window.pageYOffset >= stickyProfile) {
+//     navbarProfile.classList.add("sticky")
+//   } else {
+//     navbarProfile.classList.remove("sticky");
+//   }
+// }
+
